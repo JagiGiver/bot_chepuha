5ae11cf4ad77b58fc34fd0cfd78e5dd31a616772
John Bebra <john-bebra@john-bebra-HP.lan>
2026-06-14 17:19:33 +0300

    fix app

--- bot.py
+++ bot.py
@@ -167,7 +167,6 @@ async def receive_key(update, context):
                         reply_markup=InlineKeyboardMarkup(keyboard)
                     )
 
-app = ApplicationBuilder().token("token").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button))
 
