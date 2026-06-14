ce12eb95eba01a55296fe94117de0df8e13ef480
John Bebra <john-bebra@john-bebra-HP.lan>
2026-06-14 17:35:42 +0300

    another fix app

--- bot.py
+++ bot.py
@@ -170,7 +170,7 @@ import os
 app = ApplicationBuilder().token(os.environ["TOKEN"]).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button))
-
+app.add_handler(MessageHandler(filters.TEXT, receive_key))
 
 print("Бот запущен")
 app.run_polling()
